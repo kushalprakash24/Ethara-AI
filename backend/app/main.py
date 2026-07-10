abd1a0b53bccbc164635d4c6d78fceb901b16ce6
Lappy Garage <lappygarage@Pratham.local>
2026-07-10 21:08:54 +0530

    origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,11 +15,6 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "http://192.168.1.7:3000"  # 👈 Jo aapka local network IP terminal me dikh rha tha, wo bhi add kar diya h
-    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
